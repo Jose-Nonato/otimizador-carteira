2bd9a588b8c637e81865fcec883572257bba5a0d
JoseJunior <beijallote@gmail.com>
2023-10-06 16:40:56 -0300

    feat: atualizando estrutura

--- app.py
+++ app.py
@@ -25,29 +25,33 @@ def dash():
             colunas_removidas.append(coluna)
             dados.drop(coluna, axis=1, inplace=True)
     
-    fig_fechamento = RentabilidadeTickers(dados) # Criação do gráfico de fechamento
+    try:
+        fig_fechamento = RentabilidadeTickers(dados) # Criação do gráfico de fechamento
 
-    fig_retorno = RetornoDiarioTickers(dados) # Retorno diário dos tickers
+        fig_retorno = RetornoDiarioTickers(dados) # Retorno diário dos tickers
 
-    fig_matriz_cov, fig_estimativa_reducao, fig_retorno_estimado, fig_pesos_otimizados = MatrizCov(dados)
+        fig_matriz_cov, fig_estimativa_reducao, fig_retorno_estimado, fig_pesos_otimizados = MatrizCov(dados)
 
-    frase = VerboseCarteira(dados)
+        frase = VerboseCarteira(dados)
 
-    fig_rentabilidade = RentabilidadeRetorno(dados)
+        fig_rentabilidade = RentabilidadeRetorno(dados)
 
-    frase_eficiente = VerboseCarteiraEficiente(dados)
+        frase_eficiente = VerboseCarteiraEficiente(dados)
 
-    return render_template('dash.html', colunas_removidas=colunas_removidas, 
-                           plot_fechamento=fig_fechamento.to_html(full_html=False),
-                           plot_retorno=fig_retorno.to_html(full_html=False),
-                           plot_matriz_cov=fig_matriz_cov.to_html(full_html=False),
-                           plot_estimativa_reducao=fig_estimativa_reducao.to_html(full_html=False),
-                           plot_retorno_estimado=fig_retorno_estimado.to_html(full_html=False),
-                           plot_pesos_otimizados=fig_pesos_otimizados.to_html(full_html=False),
-                           frase=frase,
-                           plot_rentabilidade=fig_rentabilidade.to_html(full_html=False),
-                           frase_eficiente=frase_eficiente
-                           )
+        return render_template('dash.html', colunas_removidas=colunas_removidas, 
+                            plot_fechamento=fig_fechamento.to_html(full_html=False),
+                            plot_retorno=fig_retorno.to_html(full_html=False),
+                            plot_matriz_cov=fig_matriz_cov.to_html(full_html=False),
+                            plot_estimativa_reducao=fig_estimativa_reducao.to_html(full_html=False),
+                            plot_retorno_estimado=fig_retorno_estimado.to_html(full_html=False),
+                            plot_pesos_otimizados=fig_pesos_otimizados.to_html(full_html=False),
+                            frase=frase,
+                            plot_rentabilidade=fig_rentabilidade.to_html(full_html=False),
+                            frase_eficiente=frase_eficiente
+                            )
+    
+    except Exception as ex:
+        return "A seleção da carteira não superou a taxa livre de risco!"
 
 if __name__ == "__main__":
     app.run(debug=True)
